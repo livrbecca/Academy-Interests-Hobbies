d8582f83aa63ecbd34bee30abed3231439a0f8f4
livrbecca <69110329+livrbecca@users.noreply.github.com>
2021-07-16 15:27:45 +0100

    moved map html to a function

--- src/components/PeopleList.tsx
+++ src/components/PeopleList.tsx
@@ -5,7 +5,7 @@ import SearchBar from "./SearchBar";
 const PeopleList: React.FC<PeopleListProps> = ({
   displayedPeople,
   setPeople,
-}): JSX.Element => {
+}) => {
   const [searchTerm, setSearchTerm] = useState<string>("");
 
   const searchBar = displayedPeople.filter(
@@ -13,6 +13,23 @@ const PeopleList: React.FC<PeopleListProps> = ({
       person.name.toLocaleLowerCase().includes(searchTerm) ||
       person.notes?.toLocaleLowerCase().includes(searchTerm)
   );
+
+  const renderHTMLMap = (): JSX.Element[] => {
+    return searchBar.map((person) => {
+      return (
+        <>
+          <li className="List" style={{ listStyle: "none" }} key={person.name}>
+            <div className="List-header">
+              <img className="List-img" src={person.url} alt={person.name} />
+              <h2>{person.name}</h2>
+            </div>
+            <p>{person.pod} Pod</p>
+            <p className="List-note">{person.notes}</p>
+          </li>
+        </>
+      );
+    });
+  };
   return (
     <>
       <SearchBar
@@ -21,25 +38,7 @@ const PeopleList: React.FC<PeopleListProps> = ({
         searchTerm={searchTerm}
         displayedPeople={displayedPeople}
       />
-      <ul className="mainUL">
-        {searchBar.map((person) => {
-          return (
-            <li
-              // attributes, including inline CSS
-              className="List"
-              style={{ listStyle: "none" }}
-              key={person.name}
-            >
-              <div className="List-header">
-                <img className="List-img" src={person.url} alt={person.name} />
-                <h2>{person.name}</h2>
-              </div>
-              <p>{person.pod} Pod</p>
-              <p className="List-note">{person.notes}</p>
-            </li>
-          );
-        })}
-      </ul>
+      <ul className="mainUL">{renderHTMLMap()}</ul>
     </>
   );
 };
